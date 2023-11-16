784627493b8f6fcec14fa08cf6b549ee7197e4ec
BAD-EiZA <badapplestd@gmail.com>
2023-11-16 12:58:00 +0700

    Make Driver Controller & API

--- rest-api/routes/api.php
+++ rest-api/routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\DriverController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [LoginController::class,'submit']);
-Route::post('/login/verify', [LoginController::class,'verify']);
+Route::post('/login', [LoginController::class, 'submit']);
+Route::post('/login/verify', [LoginController::class, 'verify']);
+Route::group(['middleware' => 'auth:sactum'], function () {
+    Route::get('/driver', [DriverController::class,'show']);
+    Route::post('/driver', [DriverController::class,'update']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
