e41cc420524936fa2cd6ce8c928d7138373f6bcf
BAD-EiZA <badapplestd@gmail.com>
2023-11-16 11:35:17 +0700

    Twillio Install

---
+++ rest-api/app/Http/Controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller{
+    public function submit(Request $request){
+        $request->validate([
+            'phone' => 'required|numeric|min:10'
+        ]);
+
+        $user = User::firstOrCreate([
+            'phone'=> $request->phone
+        ]);
+
+        if(!$user){
+            return response()->json(['message' => 'Could not process a user with that phone number.'], 401);
+
+        }
+
+        $user->notify();
+    }
+}
---
+++ rest-api/app/Notifications/LoginNeedsVerification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\Twilio\TwilioChannel;
+use NotificationChannels\Twilio\TwilioSmsMessage;
+
+class LoginNeedsVerification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [TwilioChannel::class];
+    }
+
+    public function toTwillio($notifiable){
+        $loginCode = rand(11111,99999);
+        return (new TwilioSmsMessage())->content("Your Login Code Is {$loginCode}, don't share to anyone");
+    }
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
