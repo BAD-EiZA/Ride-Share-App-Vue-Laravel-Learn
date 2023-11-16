98d657801a0088215def67987a21e75b5f4cd225
BAD-EiZA <badapplestd@gmail.com>
2023-11-16 13:10:48 +0700

    Update Trip Controller Show

--- rest-api/app/Http/Controllers/TripController.php
+++ rest-api/app/Http/Controllers/TripController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Trip;
 use Illuminate\Http\Request;
 
 class TripController extends Controller
@@ -18,5 +19,20 @@ class TripController extends Controller
             'destination',
             'destination_name'
         ]));
+    }
+
+    public function show(Request $request, Trip $trip){
+        if($trip->user->id == $request->user()->id){
+
+            return $trip;
+        };
+        if($trip->driver && $request->user()->driver){
+
+            if($trip->driver->id == $request->user()->driver->id){
+                return $trip;
+            }
+        }
+
+        return response()->json(['message' => 'Cannot Find This Trip'], 404);
     }
 }
